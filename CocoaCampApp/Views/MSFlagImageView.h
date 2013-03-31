2b31053b865cb25d726dc36fa0724273e5e1bac0
Michal Smialko <smialko@futuresimple.com>
2013-03-31 17:42:31 +0200

    Added gradient above flag in currency summary view.

---
+++ CocoaCampApp/Views/MSFlagImageView.h
@@ -0,0 +1,13 @@
+//
+//  MSFlagImageView.h
+//  CocoaCampApp
+//
+//  Created by Michał Śmiałko on 31.03.2013.
+//  Copyright (c) 2013 MS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSFlagImageView : UIImageView
+
+@end
---
+++ CocoaCampApp/Views/MSFlagImageView.m
@@ -0,0 +1,43 @@
+//
+//  MSFlagImageView.m
+//  CocoaCampApp
+//
+//  Created by Michał Śmiałko on 31.03.2013.
+//  Copyright (c) 2013 MS. All rights reserved.
+//
+
+#import "MSFlagImageView.h"
+#import <QuartzCore/QuartzCore.h>
+
+@interface MSFlagImageView ()
+
+@property (nonatomic, strong) CAGradientLayer *gradientLayer;
+
+@end
+
+@implementation MSFlagImageView
+
+- (void)awakeFromNib
+{
+    // Gradient layer
+    self.gradientLayer = [[CAGradientLayer alloc] init];
+    self.gradientLayer.frame = self.bounds;
+    self.gradientLayer.colors = @[(id)[[[UIColor blackColor] colorWithAlphaComponent:0.7] CGColor],
+                                  (id)[[[UIColor blackColor] colorWithAlphaComponent:0.6] CGColor],
+                                  (id)[[[UIColor blackColor] colorWithAlphaComponent:0.0] CGColor]];
+    self.gradientLayer.transform = CATransform3DRotate(self.gradientLayer.transform, M_PI_2, 0, 0, 1);
+    [self.layer addSublayer:self.gradientLayer];
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+
+    CGRect halfOfBoundRect = CGRectMake(0,
+                                        0,
+                                        self.bounds.size.width/2.,
+                                        self.bounds.size.height);
+    self.gradientLayer.frame = CGRectOffset(halfOfBoundRect, self.bounds.size.width/2., 0);
+}
+
+@end
